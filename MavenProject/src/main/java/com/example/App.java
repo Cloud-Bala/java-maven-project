b2619e421d34e34ad0c8ed61de2a7aa05284b90e
Balamurugan <bsakthivel@tonikbank.com>
2024-04-04 01:46:05 +0530

    2nd project

--- MavenProject/src/main/java/com/example/App.java
+++ MavenProject/src/main/java/com/example/App.java
@@ -1,4 +1,4 @@
-package Notify-Function;
+package com.example;
 
 /**
  * Hello world!
--- MavenProject/src/test/java/com/example/AppTest.java
+++ MavenProject/src/test/java/com/example/AppTest.java
@@ -1,4 +1,4 @@
-package Notify-Function;
+package com.example;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
