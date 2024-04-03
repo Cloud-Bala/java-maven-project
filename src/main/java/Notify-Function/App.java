1a3a1b526ac76faa7e32c8d6ac1227b58df834ff
Balamurugan <bsakthivel@tonikbank.com>
2024-04-03 23:52:21 +0530

    first-project-file-push

---
+++ src/main/java/Notify-Function/App.java
@@ -0,0 +1,13 @@
+package Notify-Function;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
